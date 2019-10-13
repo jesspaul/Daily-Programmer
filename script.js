8a1040d80e5274a4d0dbb673a910052d8a5a18ac
Jessica Paul <paul.jessica95@gmail.com>
2019-10-13 12:01:21 -0400

    Completed challenge 372
    Given a string containing only the characters x and y, find whether there are the same number of xs and ys. [2019-01-14]

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// [2019-02-11] Challenge #375 [Easy] Print a new number by adding one to each of its digit
+/* // [2019-02-11] Challenge #375 [Easy] Print a new number by adding one to each of its digit
 // https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/
 
 const addOne = input => {
@@ -12,4 +12,49 @@ const addOne = input => {
 };
 
 console.log(addOne(456));
-console.log(typeof addOne(3456));
+console.log(typeof addOne(3456)); */
+
+// [2019-01-14] Challenge #372 [Easy] Perfectly balanced
+// Given a string containing only the characters x and y, find whether there are the same number of xs and ys.
+// https://www.reddit.com/r/dailyprogrammer/comments/afxxca/20190114_challenge_372_easy_perfectly_balanced/
+
+const numOfX = input => {
+    let splitInput = input.split('');
+    let xCounter = 0;
+    splitInput.forEach(function(element) {
+        if (element === 'x') {
+            xCounter++;
+        }
+    })
+    return xCounter;
+}
+
+const numOfY = input => {
+    let splitInput = input.split('');
+    let yCounter = 0;
+    splitInput.forEach(function(element) {
+        if (element === 'y') {
+            yCounter++;
+        }
+    })
+    return yCounter;
+}
+
+
+const balanced = input => {
+    if (input.length %2 !== 0) {
+        return false;
+    } else if (numOfX(input) === numOfY(input)) {
+        return true;
+    } else {
+        return false;
+    }
+    };
+
+console.log(balanced('xxxyyy'));
+console.log(balanced('yyyxxx'));
+console.log(balanced('xxxyyyy'));
+console.log(balanced('yyxyxxyxxyyyyxxxyxyx'));
+console.log(balanced('xyxxxxyyyxyxxyxxyy'));
+console.log(balanced(''));
+console.log(balanced('x'));
