820f4b224fe05f6bd9a5e712739a2ef60a016dab
Jessica Paul <paul.jessica95@gmail.com>
2019-10-17 20:54:12 -0400

    Completed challenge 366
    Given two strings of letters, determine whether the second can be made from the first by removing one letter. The remaining letters must stay in the same order. [2018-08-20]

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(typeof addOne(3456)); */
 // Given a string containing only the characters x and y, find whether there are the same number of xs and ys.
 // https://www.reddit.com/r/dailyprogrammer/comments/afxxca/20190114_challenge_372_easy_perfectly_balanced/
 
-const numOfX = input => {
+/* const numOfX = input => {
     let splitInput = input.split('');
     let xCounter = 0;
     splitInput.forEach(function(element) {
@@ -40,7 +40,6 @@ const numOfY = input => {
     return yCounter;
 }
 
-
 const balanced = input => {
     if (input.length %2 !== 0) {
         return false;
@@ -57,4 +56,30 @@ console.log(balanced('xxxyyyy'));
 console.log(balanced('yyxyxxyxxyyyyxxxyxyx'));
 console.log(balanced('xyxxxxyyyxyxxyxxyy'));
 console.log(balanced(''));
-console.log(balanced('x'));
+console.log(balanced('x')); */
+
+// [2018-08-20] Challenge #366 [Easy] Word funnel 1
+// Given two strings of letters, determine whether the second can be made from the first by removing one letter. The remaining letters must stay in the same order.
+// https://www.reddit.com/r/dailyprogrammer/comments/98ufvz/20180820_challenge_366_easy_word_funnel_1/
+
+const funnel = (firstWord, secondWord) => {
+    let compare = '';
+    for (let i = 0; i < firstWord.length; i++) {
+        let firstHalf = firstWord.substring(0, i-1);
+        let secondHalf = firstWord.substring(i);
+        compare = firstHalf+secondHalf;
+        if (compare == secondWord) {
+            return true;
+        }
+    }
+    return false;
+    
+};
+
+console.log(funnel('smerf', 'merf'));
+console.log(funnel("leave", "eave"));
+console.log(funnel("reset", "rest"));
+console.log(funnel("dragoon", "dragon"));
+console.log(funnel("eave", "leave"));
+console.log(funnel("sleet", "lets"));
+console.log(funnel("skiff", "ski"));
