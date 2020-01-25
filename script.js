78cc8607ff53d65081d97994eba51cf498636d01
Jessica Paul <paul.jessica95@gmail.com>
2020-01-25 15:36:35 -0500

    Completed challenge 381
    Your task is to find the maximum possible score for this roll in the upper section of the Yahtzee score card. [2019-11-11]

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ console.log(balanced('xyxxxxyyyxyxxyxxyy'));
 console.log(balanced(''));
 console.log(balanced('x')); */
 
-// [2018-08-20] Challenge #366 [Easy] Word funnel 1
+/*// [2018-08-20] Challenge #366 [Easy] Word funnel 1
 // Given two strings of letters, determine whether the second can be made from the first by removing one letter. The remaining letters must stay in the same order.
 // https://www.reddit.com/r/dailyprogrammer/comments/98ufvz/20180820_challenge_366_easy_word_funnel_1/
 
@@ -82,4 +82,42 @@ console.log(funnel("reset", "rest"));
 console.log(funnel("dragoon", "dragon"));
 console.log(funnel("eave", "leave"));
 console.log(funnel("sleet", "lets"));
-console.log(funnel("skiff", "ski"));
+console.log(funnel("skiff", "ski")); */
+
+//[2019-11-11] Challenge #381 [Easy] Yahtzee Upper Section Scoring
+//Your task is to find the maximum possible score for this roll in the upper section of the Yahtzee score card.
+//https://www.reddit.com/r/dailyprogrammer/comments/dv0231/20191111_challenge_381_easy_yahtzee_upper_section/
+
+const yahtzeeScore = (arr) => {
+    let oneCount = 0;
+    let twoCount = 0;
+    let threeCount = 0;
+    let fourCount = 0;
+    let fiveCount = 0;
+    let sixCount = 0;
+
+    arr.forEach(element => {if (element === 1) {
+        oneCount++;
+    } else if (element === 2) {
+        twoCount++;
+    } else if (element === 3) {
+        threeCount++;
+    } else if (element === 4) {
+        fourCount++;
+    } else if (element === 5) {
+        fiveCount++;
+    } else {
+        sixCount++;
+    };})
+    
+    let oneScore = oneCount * 1;
+    let twoScore = twoCount * 1;
+    let threeScore = threeCount * 3;
+    let fourScore = fourCount * 4;
+    let fiveScore = fiveCount * 5;
+    let sixScore = sixCount * 6;
+
+    return Math.max(oneScore, twoScore, threeScore, fourScore, fiveScore, sixScore);
+};
+
+console.log(yahtzeeScore([1, 2, 3, 3, 4]))
